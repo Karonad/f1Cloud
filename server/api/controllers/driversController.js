1fcc4b9952a7368179ecae503cab9b0102f7fd80
pacski <pachocinskii@gmail.com>
2021-11-25 15:08:49 +0100

    add intro

--- server/api/controllers/driversController.js
+++ server/api/controllers/driversController.js
@@ -8,12 +8,18 @@ exports.getData = function(req, res) {
         "frames": [],
     };
     driversRepo.listAllDrivers().then(drivers => {
+        let introDriver = { "text": "Top 5 pilotes", "icon": 12755 };
+        data.frames.push(introDriver);
         let tmpD = {"text": drivers, "icon": 12755};
         data.frames.push(tmpD);
         constructorRepo.getConstructorRanking().then(constructors => {
+            let introConstructor = { "text": "Top 5 constructeurs", "icon": 12755 };
+            data.frames.push(introConstructor);
             let tmpC = {"text": constructors, "icon": 12755};
             data.frames.push(tmpC);
             eventRepo.getNextGp().then(nextGp => {
+                let introNextGp = { "text": "Prochain Grand Prix", "icon": 12755 };
+                data.frames.push(introNextGp);
                 let tmpNgp = { "text": nextGp, "icon": 12755 };
                 data.frames.push(tmpNgp)
                 res.json(data);
